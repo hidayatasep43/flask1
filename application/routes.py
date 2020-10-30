e81e73fd23acca4c98c0ee3c765382a6d7d7b902
Asep Hidayat <hidayatasep43@gmail.com>
2020-10-31 06:57:36 +0700

    setup for login

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from application import app, db
-from flask import render_template, request, json, Response
-from application.models import user, Course, Enrollment
+from flask import render_template, request, json, Response, redirect, flash
+from application.models import User, Course, Enrollment
 from application.forms import LoginForm, RegisterForm
 
 courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
@@ -14,11 +14,21 @@ def index():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template("login.html", title = "Login ey", form = form, login = True)
+    if form.validate_on_submit(): 
+        if request.form.get("email") == "test@uta.com": 
+            flash("You are successfully logged in!", "success") 
+            return redirect("/index")
+        else : 
+            flash("Sorry, something went wrong", "danger") 
+    return render_template("login.html", title = "Login", form = form, login = True)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html", register = True)
+    form = RegisterForm()
+    if form.validate_on_submit():  
+        flash(form.email, "danger") 
+    
+    return render_template("register.html", title = "Register New Account", form = form, register = True)
 
 @app.route("/courses/")
 @app.route("/courses/<term>")
